c040b2d295f60b2c4eadd81803391121c89ae0e4
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-09 15:48:53 +0530

    Implement increment button functionality

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const counterElement = document.querySelector(".counter");
+const incrementElement = document.querySelector(".increment");
 
 let count = 1000;
 displayCount();
 function displayCount() {
   counterElement.innerHTML = `Count: ${count}`;
 }
+
+incrementElement.addEventListener("click", () => {
+  count++;
+  displayCount();
+});
