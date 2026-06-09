ecf7563bb06ed7527daed483a60be2ec648e35d7
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-09 15:54:03 +0530

    Implement decrement button functionality

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const counterElement = document.querySelector(".counter");
 const incrementElement = document.querySelector(".increment");
+const decrementElement = document.querySelector(".decrement");
 
 let count = 1000;
 displayCount();
+
 function displayCount() {
   counterElement.innerHTML = `Count: ${count}`;
 }
 
 incrementElement.addEventListener("click", () => {
   count++;
+  displayCount();
+});
+
+decrementElement.addEventListener("click", () => {
+  count--;
   displayCount();
 });
